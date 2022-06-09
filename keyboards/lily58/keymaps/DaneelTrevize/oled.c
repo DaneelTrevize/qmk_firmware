f8da4e1186118d5c34c6e45a6690470347158293
Daneel Trevize <evedaneeltrevize@gmail.com>
2022-06-09 09:51:56 +0100

    New Modifiers state art. New Locks state art.

--- keyboards/lily58/keymaps/DaneelTrevize/oled.c
+++ keyboards/lily58/keymaps/DaneelTrevize/oled.c
@@ -205,48 +205,38 @@ const char mirr_num_bmp[] PROGMEM = {
 	0x00
 };
 
-const char led_num_bmp[] PROGMEM = {
-	// Num
-	0b00111111,
-	0b00000100,
-	0b00001000,
-	0b00111111,
-	0x00,
-	0b00111100,
-	0b00100000,
-	0b00111100,
-	0x00,
-	0b00111100,
-	0b00000100,
-	0b00111100,
-	0b00000100,
-	0b00111100
-};
-
 const char led_caps_bmp[] PROGMEM = {
-	// Cap
-	0b00111111,
-	0b00100001,
-	0b00110011,
-	0x00,
-	0b00111100,
-	0b00010100,
-	0b00111100,
-	0x00,
-	0b00111100,
-	0b00010100,
-	0b00011100
+	// A
+	0b01111110,
+	0b01111111,
+	0b00001001,
+	0b00001001,
+	0b01111111,
+	0b01111110
 };
 
 const char led_scroll_bmp[] PROGMEM = {
-	// Sc
-	0b00100111,
-	0b00111101,
-	0x00,
-	0b00111100,
-	0b00100100
+	// Padlock icon
+	0b01110000,
+	0b01110110,
+	0b01010001,
+	0b01010001,
+	0b01010001,
+	0b01010001,
+	0b01110110,
+	0b01110000
 };
 
+const char led_num_bmp[] PROGMEM = {
+	// 1
+	0b01000000,
+	0b01000100,
+	0b01111110,
+	0b01111111,
+	0b01000000,
+	0b01000000
+};
+/*
 const char shift_bmp[] PROGMEM = {
 	// Shift
 	0b01000110,
@@ -309,6 +299,107 @@ const char gui_bmp[] PROGMEM = {
 	0b01111110,
 	0b01000010,
 };
+*/
+
+const uint8_t MOD_BMP_SIZE = 10;
+const uint8_t M_B_UP_B_SIZE = 2;	// sizeof(mods_upper_before_bmp);
+const uint8_t M_B_UP_A_SIZE = 2;	// sizeof(mods_upper_after_bmp);
+const uint8_t MOD_BMP_UP_SIZE = MOD_BMP_SIZE - M_B_UP_B_SIZE - M_B_UP_A_SIZE;
+const uint8_t M_B_LO_B_SIZE = 2;	// sizeof(mods_lower_before_bmp);
+const uint8_t M_B_LO_A_SIZE = 2;	// sizeof(mods_lower_after_bmp);
+const uint8_t MOD_BMP_LO_SIZE = MOD_BMP_SIZE - M_B_LO_B_SIZE - M_B_LO_A_SIZE;
+
+const char mods_upper_before_bmp[] PROGMEM = {
+	0b11111110,
+	0b11111111
+};
+
+const char ctrl_upper_bmp[] PROGMEM = {
+	0b00000111,
+	0b00000011,
+	0b11110011,
+	0b11110011,
+	0b11110011,
+	0b11110011
+};
+
+const char alt_upper_bmp[] PROGMEM = {
+	0b00000111,
+	0b00000011,
+	0b01110001,
+	0b01110001,
+	0b00000011,
+	0b00000111
+};
+
+const char shift_upper_bmp[] PROGMEM = {
+	0b10000111,
+	0b00000011,
+	0b00110011,
+	0b01110011,
+	0b01110011,
+	0b11110011
+};
+
+const char altgr_upper_bmp[] PROGMEM = {
+	0b00000111,
+	0b00000011,
+	0b11110011,
+	0b01110011,
+	0b01110011,
+	0b01110011
+};
+
+const char mods_upper_after_bmp[] PROGMEM = {
+	0b11111111,
+	0b11111110
+};
+
+const char mods_lower_before_bmp[] PROGMEM = {
+	0b00111111,
+	0b01111111
+};
+
+const char ctrl_lower_bmp[] PROGMEM = {
+	0b01110000,
+	0b01100000,
+	0b01100111,
+	0b01100111,
+	0b01100111,
+	0b01100111
+};
+
+const char alt_lower_bmp[] PROGMEM = {
+	0b01100000,
+	0b01100000,
+	0b01111110,
+	0b01111110,
+	0b01100000,
+	0b01100000
+};
+
+const char shift_lower_bmp[] PROGMEM = {
+	0b01100111,
+	0b01100111,
+	0b01100111,
+	0b01100110,
+	0b01100000,
+	0b01110000
+};
+
+const char altgr_lower_bmp[] PROGMEM = {
+	0b01110000,
+	0b01100000,
+	0b01100111,
+	0b01100110,
+	0b01100000,
+	0b01100000
+};
+
+const char mods_lower_after_bmp[] PROGMEM = {
+	0b01111111,
+	0b00111111
+};
 
 void clear_range(uint16_t index, uint16_t size) {
 	for( uint16_t i = 0; i < size; i++) {
@@ -346,13 +437,14 @@ void write_or_clear( bool write_not_clear, uint16_t index, const char *data, uin
 }
 
 void render_host_led_state( uint16_t index ) {
-	write_or_clear( IS_HOST_LED_ON(USB_LED_NUM_LOCK), index, led_num_bmp, sizeof(led_num_bmp) );
-	index += sizeof(led_num_bmp) + 1;
+	index += 2;
+	write_or_clear( !IS_HOST_LED_ON(USB_LED_SCROLL_LOCK), index, led_scroll_bmp, sizeof(led_scroll_bmp) );
+	index += sizeof(led_scroll_bmp) + 4;
 	write_or_clear( IS_HOST_LED_ON(USB_LED_CAPS_LOCK), index, led_caps_bmp, sizeof(led_caps_bmp) );
-	index += sizeof(led_caps_bmp) + 1;
-	write_or_clear( IS_HOST_LED_ON(USB_LED_SCROLL_LOCK), index, led_scroll_bmp, sizeof(led_scroll_bmp) );
+	index += sizeof(led_caps_bmp) + 4;
+	write_or_clear( IS_HOST_LED_ON(USB_LED_NUM_LOCK), index, led_num_bmp, sizeof(led_num_bmp) );
 }
-
+/*
 void render_mod_state( uint16_t index ) {
 	// Should check passed index has enough room left to fit all the lines..?
 	
@@ -364,5 +456,33 @@ void render_mod_state( uint16_t index ) {
 	write_or_clear( mods & MOD_BIT(KC_LEFT_ALT), index+64, alt_bmp, sizeof(alt_bmp) );
 	write_or_clear( mods & MOD_BIT(KC_RIGHT_ALT), index+96-sizeof(alt_bmp), alt_bmp, sizeof(alt_bmp) );
 	write_or_clear( mods & MOD_MASK_GUI, index+64+sizeof(alt_bmp)+1, gui_bmp, sizeof(gui_bmp) );
+}
+*/
+void write_mod_or_clear( bool write_not_clear, uint16_t index, const char *upper_bmp, const char *lower_bmp ) {
+	if( write_not_clear ) {
+		oled_write_data_P( mods_upper_before_bmp, index, M_B_UP_B_SIZE );
+		oled_write_data_P( upper_bmp, index + M_B_UP_B_SIZE, MOD_BMP_UP_SIZE );
+		oled_write_data_P( mods_upper_after_bmp, index + M_B_UP_B_SIZE + MOD_BMP_UP_SIZE, M_B_UP_A_SIZE );
+		
+		oled_write_data_P( mods_lower_before_bmp, index + 32, M_B_LO_B_SIZE );
+		oled_write_data_P( lower_bmp, index + 32 + M_B_LO_B_SIZE, MOD_BMP_LO_SIZE );
+		oled_write_data_P( mods_lower_after_bmp, index + 32 + M_B_LO_B_SIZE + MOD_BMP_LO_SIZE, M_B_LO_A_SIZE );
+	} else {
+		clear_range( index, MOD_BMP_SIZE );
+		clear_range( index + 32, MOD_BMP_SIZE );
+	}
+}
+
+void render_mod_state( uint16_t index ) {
+	// Should check passed index has enough room left to fit all the lines..?
+	
+	uint8_t mods = get_mods() | get_oneshot_mods();
+	write_mod_or_clear( mods & MOD_BIT(KC_LEFT_CTRL), index, ctrl_upper_bmp, ctrl_lower_bmp );
+	write_mod_or_clear( mods & MOD_BIT(KC_LEFT_ALT), index + MOD_BMP_SIZE + 1, alt_upper_bmp, alt_lower_bmp );
+	write_mod_or_clear( mods & MOD_BIT(KC_RIGHT_CTRL), index + (MOD_BMP_SIZE + 1)*2, ctrl_upper_bmp, ctrl_lower_bmp );
+	index += 64;
+	write_mod_or_clear( mods & MOD_BIT(KC_LEFT_SHIFT), index, shift_upper_bmp, shift_lower_bmp );
+	write_mod_or_clear( mods & MOD_BIT(KC_RIGHT_ALT), index + MOD_BMP_SIZE + 1, altgr_upper_bmp, altgr_lower_bmp );
+	write_mod_or_clear( mods & MOD_BIT(KC_RIGHT_SHIFT), index + (MOD_BMP_SIZE + 1)*2, shift_upper_bmp, shift_lower_bmp );
 }
 #endif  // OLED_ENABLE
